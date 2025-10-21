ab2e7166e27e90f0d6813c0aac50f7f77e988148
tejasM17 <tejasm9380@gmail.com>
2025-10-21 14:48:25 +0530

    initial project started

---
+++ Travel-web/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='bg-gray-300'>
+        <h1 className='text-5xl text-center'>Travelx 💕</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
